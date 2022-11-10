ff8bc0724a0331b7a3c316855cd3de4742d11f28
SimonHung8 <simon850226@gmail.com>
2022-11-10 22:46:14 +0800

    feat: project init

---
+++ app.js
@@ -0,0 +1,21 @@
+// require packages
+const express = require('express')
+const { engine } = require('express-handlebars')
+const methodOverride = require('method-override')
+const bcrypt = require('bcryptjs')
+
+const app = express()
+const PORT = 3000
+
+// template engine
+app.engine('hbs', engine({ defaultLayout: 'main', extname: 'hbs' }))
+app.set('view engine', 'hbs')
+
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
+
+app.get('/', (req, res) => {
+  res.send('project TODO SEQUELIZE init')
+})
+
+app.listen(PORT, (req, res) => console.log(`App is running on http://localhost:${PORT}`))
