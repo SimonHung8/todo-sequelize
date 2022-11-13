2eef6d460e930db763f2334739df09dfceb37e76
SimonHung8 <simon850226@gmail.com>
2022-11-13 21:58:13 +0800

    feat: update register feature

---
+++ middleware/validator.js
@@ -0,0 +1,29 @@
+const { body } = require('express-validator')
+const db = require('../models')
+const User = db.User
+
+module.exports = {
+  registerValidator: [
+    body('name').not().isEmpty().withMessage('請輸入使用者名稱'),
+    body('email').isEmail().withMessage('請輸入有效Email')
+      .bail().custom(async email => {
+        try {
+          const user = await User.findOne({ where: { email } })
+          if (user) throw ('這個email已經註冊過了')
+          return true
+        } catch (err) {
+          if (err === '這個email已經註冊過了') throw new Error(err)
+          throw new Error('資料庫好像出錯了... 請過一會再使用')
+        }
+      }),
+    body('password').not().isEmpty().withMessage('請輸入密碼')
+      .bail().custom(password => {
+        if (/\s/.test(password)) throw new Error('密碼請勿輸入空白')
+        return true
+      }),
+    body('confirmPassword').custom((value, { req }) => {
+      if (value !== req.body.password) throw new Error('請輸入相同密碼')
+      return true
+    })
+  ]
+}
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -2,6 +2,8 @@ const express = require('express')
 const passport = require('passport')
 const bcrypt = require('bcryptjs')
 const router = express.Router()
+const { validationResult } = require('express-validator')
+const { registerValidator } = require('../../middleware/validator')
 const db = require('../../models')
 const User = db.User
 
@@ -18,20 +20,20 @@ router.get('/register', (req, res) => {
   res.render('register')
 })
 
-router.post('/register', (req, res, next) => {
-  const { name, email, password, confirmPassword } = req.body
-  User.findOne({ where: { email } })
-    .then(user => {
-      if (user) {
-        console.log('user already exists')
-        return res.render('register', { name, email, password, confirmPassword })
-      }
-      return bcrypt.genSalt(5)
-        .then(salt => bcrypt.hash(password, salt))
-        .then(hash => User.create({ name, email, password: hash }).catch(next))
-        .then(() => res.redirect('/'))
-        .catch(next)
-    })
+router.post('/register', registerValidator, async (req, res, next) => {
+  try {
+    const errors = validationResult(req)
+    const { name, email, password, confirmPassword } = req.body
+    if (!errors.isEmpty()) {
+      return res.render('register', { errorMsg: errors.errors, name, email, password, confirmPassword })
+    }
+    const salt = await bcrypt.genSalt(5)
+    const hash = await bcrypt.hash(password, salt)
+    const user = await User.create({ name, email, password: hash })
+    req.logIn(user, () => res.redirect('/'))
+  } catch (err) {
+    next(err)
+  }
 })
 
 module.exports = router
